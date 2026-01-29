18a60e2fdd04e8b38506960dbf660f620a3b3a28
Ivan Ceotropica <ivantecnico2020@yahoo.com>
2026-01-29 23:36:46 +0100

    Conectar boton Guardar aviso

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,42 @@
+"use client";
+console.log("PAGE CARGADA");
+import {useState} from "react";
 export default function Home() {
+const [cliente, setCliente] = useState("");
+const [maquina, setMaquina] = useState("");
+const [descripcion, setDescripcion] = useState("");
+function guardarAviso() {
+  if (!cliente || !descripcion) {
+    alert("Cliente y avería son obligatorios");
+    return;
+  }
+
+  const nuevoAviso = {
+    id: Date.now(),
+    cliente,
+    maquina,
+    descripcion,
+    tipo: "reparacion",
+    estado: "abierto",
+    fechaCreacion: new Date().toISOString(),
+  };
+
+  const avisosExistentes = JSON.parse(
+    localStorage.getItem("avisos") || "[]"
+  );
+
+  avisosExistentes.push(nuevoAviso);
+
+  localStorage.setItem("avisos", JSON.stringify(avisosExistentes));
+
+  // limpiar formulario
+  setCliente("");
+  setMaquina("");
+  setDescripcion("");
+
+  alert("Aviso guardado");
+}
+
   return (
     <main className="p-4 max-w-md mx-auto">
       <h1 className="text-2xl font-bold mb-4">CEOTROPICA · SAT</h1>
@@ -6,20 +44,28 @@ export default function Home() {
       <input
         className="border p-2 w-full mb-3"
         placeholder="Cliente"
+         value={cliente}
+         onChange={(e) => setCliente(e.target.value)}
       />
 
       <input
         className="border p-2 w-full mb-3"
         placeholder="Máquina"
+        value={maquina}
+        onChange={(e) => setMaquina(e.target.value)}
       />
 
       <textarea
         className="border p-2 w-full mb-3"
         placeholder="Avería / trabajo realizado"
         rows={4}
+        value={descripcion}
+        onChange={(e) => setDescripcion(e.target.value)}
       />
 
-      <button className="bg-black text-white w-full p-2 rounded">
+      <button 
+       onClick={guardarAviso}
+       className="bg-black text-white w-full p-2 rounded">
         Guardar aviso
       </button>
     </main>
